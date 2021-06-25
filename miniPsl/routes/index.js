3f69abafc53af2e9a689326e913d6c1165ac6fdb
Saad Zulfiqar <fa17-bcs-028@cuilahore.edu.pk>
2021-06-25 18:47:26 +0500

    2nd commit
    2nd commit

--- miniPsl/routes/index.js
+++ miniPsl/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render("homePage");
+});
+
+router.get('/AdminPage', function(req, res, next) {
+  res.render("AdminPage");
+});
+
+router.get('/totalTeams', function(req, res, next) {
+  res.render("totalTeams");
 });
 
 module.exports = router;
